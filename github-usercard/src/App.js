6420e07c03772cac3b01bdc78d40d675b7231c20
Lou <antilou86@gmail.com>
2019-08-06 15:51:24 -0400

    WIP: initial commit, built out basic wireframes, added empty component files and directory, fleshed out beginnings of App.js to include axios call

---
+++ github-usercard/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+class App extends React.Component() {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    this.fetchGitData();
+  }
+
+  fetchGitData = () => {
+    axios.get('https://api.github.com/users/antilou86').then(
+      (res) => {
+        console.log('info.data: ' + res.data);
+        this.setState({}) //unsure, lets look at the response data
+        })
+      .catch(
+        (err) => {
+          console.log('the error is: ' + err);
+        })
+  }
+
+  render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Github User Card: now with ReactJs! </h1>
+      </header>
+      <div><p>user card goes here</p></div>
+
+      <div><p>graph here</p></div>
+
+      <div><p>followers here</p></div>
+    </div>
+  );
+};
+}
+
+export default App;
